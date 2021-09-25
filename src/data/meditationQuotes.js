5eb92882227f5efdfd1a0daf7ee4be1d74d34595
Aaron Yu <ayybuddyy@gmail.com>
2021-09-25 15:36:24 -0400

    Add starter data

---
+++ src/data/meditationQuotes.js
@@ -0,0 +1,8 @@
+export const meditationQuotes = [
+  "When the mind is calm, there's clarity; with clarity there's contentment; with contentment, there's compassion.",
+  'We say we want to be happy and free, yet we often repeat the many causes of our suffering.',
+  "Have you noticed that when you're more present, you also tend to be more content?",
+  'Not allowing the past to ruffle us, or the future to distract us, we sit and appreciate each passing moment, still, quiet, present in the world.',
+  'If you only had this day, what would you do first?',
+  "Although we don't necessarily like the feeling of anger, the mind quite likes the feeling of being involved in stuff. Even anger.",
+];
---
+++ src/data/quotes.js
@@ -0,0 +1,16 @@
+export const quotes = [
+  {
+    quote: 'There are 7 days in a week and "someday" is not one of them.',
+    person: 'Benny Lewis',
+  },
+  {
+    quote:
+      "You are going to pay a price for everything you do and everything you do not do. You do not get to choose to not pay a price. You get to choose with poison you are going to take. That's it.",
+    person: 'Jordan Peterson',
+  },
+  {
+    quote:
+      'Iron rusts from disuse; stagnant water loses its purity and in cold weather becomes frozen; even so does inaction sap the vigor of the mind. So we must stretch ourselves to the very limits of human possibility. Anything less is a sin against both God and man.',
+    person: 'Leonardo da Vinci',
+  },
+];
